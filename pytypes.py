aadcf4d8d819c71f8bb799f99120c18e0c6c85c2
Dan Wang <rm.danwang@gmail.com>
2019-07-04 16:47:41 -0500

    Update pytypes.py

--- pytypes.py
+++ pytypes.py
@@ -8,3 +8,14 @@ print(type(True))
 print(type((5,'program', 1+3j)))
 print(type({5,2,3,1,4}))
 print(type({1:'value','key':2}))
+
+Output: 
+ <class 'list'>
+<class 'int'>
+<class 'str'>
+<class 'float'>
+<class 'complex'>
+<class 'bool'>
+<class 'tuple'>
+<class 'set'>
+<class 'dict'>
