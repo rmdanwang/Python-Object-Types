90b442bded211197749e27f503e56e2297ea1d52
Dan Wang <rm.danwang@gmail.com>
2019-07-04 16:44:56 -0500

    Add files via upload

---
+++ pytypes.py
@@ -0,0 +1,10 @@
+#Homework by Ru Meng(Dan) Wang pytypes.py
+print(type([47,48,49]))
+print(type(47))
+print(type("47"))
+print(type(4.7))
+print(type(4+7j))
+print(type(True))
+print(type((5,'program', 1+3j)))
+print(type({5,2,3,1,4}))
+print(type({1:'value','key':2}))
